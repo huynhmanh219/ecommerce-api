f13dc421b0bd3b8508299df0df2a7b517ca20620
huynhmanh219 <135082065+huynhmanh219@users.noreply.github.com>
2026-04-13 16:19:20 +0700

    fix add package for each service

---
+++ services/event-service/src/intities/event_store.entities.ts
@@ -0,0 +1,32 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, Tree } from "typeorm";
+
+
+@Entity('event_store')
+export class Event{
+    @PrimaryGeneratedColumn('uuid')
+    id:string;
+
+
+    @Column()
+    aggregatedId:string;
+
+
+    @Column()
+    aggregateType:string;
+
+    @Column()
+    eventType:string;
+    
+
+    @Column('jsonb')
+    data:any;
+
+    @Column({default:1})
+    version:number;
+
+    @CreateDateColumn()
+    timestamp:Date;
+
+    @Column('jsonb',{nullable:true})
+    metadata:any;
+}
